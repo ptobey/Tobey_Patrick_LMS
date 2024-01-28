43bb3c76ab9cfdd37afa4bd72219764383b25bcb
Patrick Tobey <patmictob@gmail.com>
2024-01-27 23:09:13 -0500

    Cleaned up code more

--- src/Book.java
+++ src/Book.java
@@ -8,7 +8,6 @@ Class Name: Book
 This class represents a single book.
 This class has three properties: id, title, and author.
 All three properties have getters but can only be set by the constructor.
-
  */
 public class Book {
     private final String id;
@@ -18,7 +17,7 @@ public class Book {
     /*
     The constructor used if an ID needs to be generated for the book
      */
-    public Book( String title, String author) {
+    public Book(String title, String author) {
         String newId;
 
         this.title = title;
@@ -27,7 +26,7 @@ public class Book {
         do {
             newId = String.valueOf(Main.incrementIdCounter());
             System.out.println();
-        } while(Main.getExistingIds().contains(newId));
+        } while (Main.getExistingIds().contains(newId));
 
         this.id = newId;
         Main.addExistingId(newId);
@@ -36,7 +35,7 @@ public class Book {
     /*
     The constructor used if an ID is already created for the book
      */
-    public Book( String id, String title, String author) {
+    public Book(String id, String title, String author) {
         this.title = title;
         this.author = author;
         this.id = id;
@@ -46,9 +45,11 @@ public class Book {
     public String getId() {
         return this.id;
     }
+
     public String getTitle() {
         return this.title;
     }
+
     public String getAuthor() {
         return this.author;
     }
